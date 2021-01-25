2d9b965d681b7b8f65149603007f1048be9aa3b2
mahmoudAcm <mahmoud.acm.123@gamil.com>
2021-01-25 07:51:20 +0200

    adding user module to app module & creating user Shcema

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DB_LINK } from '../../configs';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(DB_LINK)
-  ],
+  imports: [MongooseModule.forRoot(DB_LINK), UserModule],
 })
 export class AppModule {}
---
+++ src/modules/user/user.schema.ts
@@ -0,0 +1,36 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ required: true })
+  username: string;
+
+  @Prop({ required: true })
+  firstname: string;
+
+  @Prop({ required: true })
+  lastname: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  private password: string;
+
+  @Prop({ enum: ['male', 'female'] })
+  gender?: string;
+
+  @Prop()
+  phone?: string;
+
+  @Prop()
+  location?: string;
+
+  @Prop()
+  city?: string;
+
+  @Prop()
+  dateOfBirth?: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
