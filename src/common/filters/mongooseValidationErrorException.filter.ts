e77baf9c9eee1e0965304f9f6da4356e8482acfa
mahmoudAcm <mahmoud.acm.123@gamil.com>
2021-02-14 10:12:16 +0200

    adding support to ws

--- src/common/filters/mongooseValidationErrorException.filter.ts
+++ src/common/filters/mongooseValidationErrorException.filter.ts
@@ -9,9 +9,6 @@ import { Error } from 'mongoose';
 @Catch(Error)
 export class MongooseValidationErrorExceptionFilter implements ExceptionFilter {
   catch(exception: Error, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse();
-
     let errors = {};
     if (exception instanceof Error.CastError) {
       const { kind, value } = exception;
@@ -26,32 +23,41 @@ export class MongooseValidationErrorExceptionFilter implements ExceptionFilter {
     }
 
     const ErrorKeys = Object.keys(errors);
+    errors = ErrorKeys.reduce((prevState: any, fieldName: string) => {
+      switch (errors[fieldName].kind) {
+        case 'enum':
+          return {
+            ...prevState,
+            [fieldName]: `allowed values for ${fieldName} are [${(errors[
+              fieldName
+            ] as any).properties.enumValues.join(' - ')}]`,
+          };
+        case 'required':
+          return { ...prevState, [fieldName]: 'required' };
+        case 'ObjectId':
+          return {
+            ...prevState,
+            [fieldName]: `failed to cast \`${errors[fieldName].value}\` to ObjectId`,
+          };
+        default:
+          return {
+            ...prevState,
+            'Validator Error': `\`${errors[fieldName].message}`,
+          };
+      }
+    }, {});
 
-    response.status(HttpStatus.BAD_REQUEST).json({
-      status: HttpStatus.BAD_REQUEST,
-      errors: ErrorKeys.reduce((prevState: any, fieldName: string) => {
-        switch (errors[fieldName].kind) {
-          case 'enum':
-            return {
-              ...prevState,
-              [fieldName]: `allowed values for ${fieldName} are [${(errors[
-                fieldName
-              ] as any).properties.enumValues.join(' - ')}]`,
-            };
-          case 'required':
-            return { ...prevState, [fieldName]: 'required' };
-          case 'ObjectId':
-            return {
-              ...prevState,
-              [fieldName]: `failed to cast \`${errors[fieldName].value}\` to ObjectId`,
-            };
-          default:
-            return {
-              ...prevState,
-              'Validator Error': `\`${errors[fieldName].message}`,
-            };
-        }
-      }, {}),
-    });
+    if (host.getType() === 'ws') {
+      const ctx = host.switchToWs();
+      const socket = ctx.getClient();
+      socket.emit('exception', errors);
+    } else if (host.getType() === 'http') {
+      const ctx = host.switchToHttp();
+      const response = ctx.getResponse();
+      response.status(HttpStatus.BAD_REQUEST).json({
+        status: HttpStatus.BAD_REQUEST,
+        errors,
+      });
+    }
   }
 }
